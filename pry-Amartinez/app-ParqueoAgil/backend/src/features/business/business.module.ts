07ce4e4174b11f383edc6d8ad1dae1d0da9cb2b3
AderMartinez <adermartinez123@gmail.com>
2026-09-17 23:23:18 -0500

    feat(business): registrar TicketsModule en BusinessModule (ParqueoÁgil)
    Refs #6

--- pry-Amartinez/app-ParqueoAgil/backend/src/features/business/business.module.ts
+++ pry-Amartinez/app-ParqueoAgil/backend/src/features/business/business.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module.js';
 import { VehicleTypesModule } from './vehicle-types/vehicle-types.module.js';
 import { ParkingZonesModule } from './parking-zones/parking-zones.module.js';
+import { TicketsModule } from './tickets/tickets.module.js';
 
 @Module({
-  imports: [ClientsModule, VehicleTypesModule, ParkingZonesModule],
-  exports: [ClientsModule, VehicleTypesModule, ParkingZonesModule],
+  imports: [ClientsModule, VehicleTypesModule, ParkingZonesModule, TicketsModule],
+  exports: [ClientsModule, VehicleTypesModule, ParkingZonesModule, TicketsModule],
 })
 export class BusinessModule {}
