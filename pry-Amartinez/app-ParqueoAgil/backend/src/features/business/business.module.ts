db0e9381bdaf2c733fecfecbbbb0d8c0fc510252
AderMartinez <adermartinez123@gmail.com>
2026-09-17 22:16:29 -0500

    feat(business): registrar VehicleTypesModule en BusinessModule (ParqueoÁgil)
    Refs #4

--- pry-Amartinez/app-ParqueoAgil/backend/src/features/business/business.module.ts
+++ pry-Amartinez/app-ParqueoAgil/backend/src/features/business/business.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module.js';
+import { VehicleTypesModule } from './vehicle-types/vehicle-types.module.js';
 
 @Module({
-  imports: [ClientsModule],
-  exports: [ClientsModule],
+  imports: [ClientsModule, VehicleTypesModule],
+  exports: [ClientsModule, VehicleTypesModule],
 })
 export class BusinessModule {}
